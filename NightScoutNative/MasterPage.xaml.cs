734e929e16aa227ae2fd257572c5f276be1369ab
Andrew Ditmer <andrew@ditmer.us>
2016-09-15 16:44:03 -0400

    Converts single page into a MasterDetailPage and adds a Hamburger menu icon

---
+++ NightScoutNative/MasterPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace NightScoutNative
+{
+	public partial class MasterPage : ContentPage
+	{
+		public MasterPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
